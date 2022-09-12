514dc14277dae7645a8e8016b2e4023642362554
Sella97 <maruseraell@gmail.com>
2022-09-12 19:32:04 +0700

    merevisi isi

--- Contoh1.php
+++ Contoh1.php
@@ -4,10 +4,10 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama Saya Marsella Prihatini
-              Saya tinggal di daerah Kaliabang Tengah
+        echo "Nama Saya Imam Nawawi
+              Saya tinggal di daerah Ciputat
               olahraga yang saya sukai adalah
-              Bermain Volly"; 
+              Bulutangkis"; 
     }
 }
 ?>
