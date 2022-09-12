c3f07f7c6dbe7590b6527d9a41289a05ec60b1b8
Sella97 <maruseraell@gmail.com>
2022-09-12 15:39:04 +0700

    Mengganti nama file

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Marsella Prihatini
+              Saya tinggal di daerah Kaliabang Tengah
+              olahraga yang saya sukai adalah
+              Bermain Volly"; 
+    }
+}
+?>
